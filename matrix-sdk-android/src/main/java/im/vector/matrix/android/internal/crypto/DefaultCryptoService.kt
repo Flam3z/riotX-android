1dc855284833cadbc39d292e6e5369e1009a9c37
Valere <valeref@matrix.org>
2020-06-03 17:58:26 +0200

    Send with held code on key requests

--- matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/crypto/DefaultCryptoService.kt
+++ matrix-sdk-android/src/main/java/im/vector/matrix/android/internal/crypto/DefaultCryptoService.kt
@@ -1298,6 +1298,9 @@ internal class DefaultCryptoService @Inject constructor(
         return cryptoStore.getSharedWithInfo(roomId, sessionId)
     }
 
+    override fun getWithHeldMegolmSession(roomId: String, sessionId: String): RoomKeyWithHeldContent? {
+        return cryptoStore.getWithHeldMegolmSession(roomId, sessionId)
+    }
     /* ==========================================================================================
      * For test only
      * ========================================================================================== */
